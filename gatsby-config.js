6dd1d99b561d9ccaee41e8ebf570619ba7129641
vijaykumar12097 <akshaykumar12097@gmail.com>
2024-10-23 22:11:27 +0400

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Faizan Abid',
-        short_name: 'Faizan Abid',
+        name: 'Vijay Kumar',
+        short_name: 'Vijay Kumar',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
